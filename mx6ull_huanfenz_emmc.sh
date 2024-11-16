c1990e0f25e2ff839b596650e81b18b34aa44e2d
perrypengwang <perrypengwang@gmail.com>
2024-11-17 02:00:53 +0800

    Modify Network driver

--- mx6ull_huanfenz_emmc.sh
+++ mx6ull_huanfenz_emmc.sh
@@ -2,4 +2,4 @@
 
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_huanfenz_emmc_defconfig
-make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j4
+make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j6
